ffd509e468c6f3820fe3629a27ea0b8c95d59d7c
Juhyung Park <qkrwngud825@gmail.com>
2022-01-03 21:14:34 +0900

    build_kernel: create and use Image.gz
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -22,6 +22,8 @@ else
 	make "$@" || exit 1
 fi
 
+pigz < arch/arm64/boot/Image > arch/arm64/boot/Image.gz &
+
 echo "Building new ramdisk"
 #remove previous ramfs files
 rm -rf '$RAMFS_TMP'*
@@ -46,8 +48,9 @@ ls -lh $RAMFS_TMP.cpio.gz
 cd $KERNELDIR
 
 echo "Making new boot image"
+wait
 mkbootimg \
-    --kernel $KERNELDIR/arch/arm64/boot/Image \
+    --kernel $KERNELDIR/arch/arm64/boot/Image.gz \
     --ramdisk $RAMFS_TMP.cpio.gz \
     --pagesize 4096 \
     --os_version     $OS \
